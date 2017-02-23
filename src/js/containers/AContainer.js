2754b0c148d3023b5c5d8bf2419f024a6cf146c1
Arttu <arekoskela@gmail.com>
2017-02-23 22:31:25 +0200

    Fixing paths for GitHub

--- src/js/containers/AContainer.js
+++ src/js/containers/AContainer.js
@@ -1,10 +1,11 @@
-import {Entity, Scene} from 'aframe-react';
 import React from 'react';
 
 class AContainer extends React.Component {
 
   // Render AFRAME HTML
   render () {
+
+    console.log("NEW")
 
     return (
       <div></div>
